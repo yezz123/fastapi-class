71a5de91715a358d76f7d91e62f815941093cae3
Yasser Tahiri <yasserth19@gmail.com>
2022-05-23 05:22:57 +0100

    feat: Add patch function ✨ (#54)

--- fastapi_class/__init__.py
+++ fastapi_class/__init__.py
@@ -2,7 +2,7 @@
     FastAPI_Class : contains classes and decorators to use FastAPI with "class based routing".
 """
 
-__version__ = "1.1.2"
+__version__ = "1.2.0"
 
 from fastapi_class import args, decorators, routable
 
