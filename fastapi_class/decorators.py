e826ca29f0d1de7aa80ab78ead67efcdab6ed1c5
Yasser Tahiri <yasserth19@gmail.com>
2022-09-29 22:13:51 +0100

    🐛 Overwrite the partial function (#55)
    * :fire: Delete Documentation
    
    * :bug: Overwrite the partial function

--- fastapi_class/decorators.py
+++ fastapi_class/decorators.py
@@ -1,3 +1,4 @@
+import inspect
 from enum import Enum
 from typing import Any, Callable, Dict, List, Optional, Sequence, Type, TypeVar, Union
 
@@ -24,13 +25,13 @@ def route(
     """
 
     def marker(method: AnyCallable) -> AnyCallable:
-        setattr(
-            method,
-            "_endpoint",
-            EndpointDefinition(
-                endpoint=method, args=RouteArgs(path=path, methods=methods, **kwargs)
-            ),
-        )
+        args = RouteArgs(path=path, methods=methods, **kwargs)
+        if args.name is None:
+            args.name = method.__name__
+        if not args.description:
+            description = inspect.cleandoc(method.__doc__ or "")
+            args.description = description or " "
+        setattr(method, "_endpoint", EndpointDefinition(endpoint=method, args=args))
         return method
 
     return marker
@@ -144,6 +145,60 @@ def post(
     )
 
 
+def patch(
+    path: str,
+    *,
+    response_model: Optional[Type[Any]] = None,
+    status_code: Optional[int] = None,
+    tags: Optional[List[Union[str, Enum]]] = None,
+    dependencies: Optional[Sequence[params.Depends]] = None,
+    summary: Optional[str] = None,
+    description: Optional[str] = None,
+    response_description: str = "Successful Response",
+    responses: Optional[Dict[Union[int, str], Dict[str, Any]]] = None,
+    deprecated: Optional[bool] = None,
+    operation_id: Optional[str] = None,
+    response_model_include: Optional[Union[SetIntStr, DictIntStrAny]] = None,
+    response_model_exclude: Optional[Union[SetIntStr, DictIntStrAny]] = None,
+    response_model_by_alias: bool = True,
+    response_model_exclude_unset: bool = False,
+    response_model_exclude_defaults: bool = False,
+    response_model_exclude_none: bool = False,
+    include_in_schema: bool = True,
+    response_class: Type[Response] = Default(JSONResponse),
+    name: Optional[str] = None,
+    callbacks: Optional[List[BaseRoute]] = None,
+    openapi_extra: Optional[Dict[str, Any]] = None,
+    **kwargs: Any,
+) -> Callable[[AnyCallable], AnyCallable]:
+    return route(
+        path,
+        methods=["PATCH"],
+        response_model=response_model,
+        status_code=status_code,
+        tags=tags,
+        dependencies=dependencies,
+        summary=summary,
+        description=description,
+        response_description=response_description,
+        responses=responses,
+        deprecated=deprecated,
+        operation_id=operation_id,
+        response_model_include=response_model_include,
+        response_model_exclude=response_model_exclude,
+        response_model_by_alias=response_model_by_alias,
+        response_model_exclude_unset=response_model_exclude_unset,
+        response_model_exclude_defaults=response_model_exclude_defaults,
+        response_model_exclude_none=response_model_exclude_none,
+        include_in_schema=include_in_schema,
+        response_class=response_class,
+        name=name,
+        callbacks=callbacks,
+        openapi_extra=openapi_extra,
+        **kwargs,
+    )
+
+
 def put(
     path: str,
     *,
@@ -227,60 +282,6 @@ def delete(
     return route(
         path,
         methods=["DELETE"],
-        response_model=response_model,
-        status_code=status_code,
-        tags=tags,
-        dependencies=dependencies,
-        summary=summary,
-        description=description,
-        response_description=response_description,
-        responses=responses,
-        deprecated=deprecated,
-        operation_id=operation_id,
-        response_model_include=response_model_include,
-        response_model_exclude=response_model_exclude,
-        response_model_by_alias=response_model_by_alias,
-        response_model_exclude_unset=response_model_exclude_unset,
-        response_model_exclude_defaults=response_model_exclude_defaults,
-        response_model_exclude_none=response_model_exclude_none,
-        include_in_schema=include_in_schema,
-        response_class=response_class,
-        name=name,
-        callbacks=callbacks,
-        openapi_extra=openapi_extra,
-        **kwargs,
-    )
-
-
-def patch(
-    path: str,
-    *,
-    response_model: Optional[Type[Any]] = None,
-    status_code: Optional[int] = None,
-    tags: Optional[List[Union[str, Enum]]] = None,
-    dependencies: Optional[Sequence[params.Depends]] = None,
-    summary: Optional[str] = None,
-    description: Optional[str] = None,
-    response_description: str = "Successful Response",
-    responses: Optional[Dict[Union[int, str], Dict[str, Any]]] = None,
-    deprecated: Optional[bool] = None,
-    operation_id: Optional[str] = None,
-    response_model_include: Optional[Union[SetIntStr, DictIntStrAny]] = None,
-    response_model_exclude: Optional[Union[SetIntStr, DictIntStrAny]] = None,
-    response_model_by_alias: bool = True,
-    response_model_exclude_unset: bool = False,
-    response_model_exclude_defaults: bool = False,
-    response_model_exclude_none: bool = False,
-    include_in_schema: bool = True,
-    response_class: Type[Response] = Default(JSONResponse),
-    name: Optional[str] = None,
-    callbacks: Optional[List[BaseRoute]] = None,
-    openapi_extra: Optional[Dict[str, Any]] = None,
-    **kwargs: Any,
-) -> Callable[[AnyCallable], AnyCallable]:
-    return route(
-        path,
-        methods=["PATCH"],
         response_model=response_model,
         status_code=status_code,
         tags=tags,
--- fastapi_class/routable.py
+++ fastapi_class/routable.py
@@ -9,14 +9,6 @@ from fastapi_class.args import EndpointDefinition
 
 AnyCallable = TypeVar("AnyCallable", bound=Callable[..., Any])
 
-"""
-    This module contains the `Routable` class, which is used to define a class
-
-    Returns:
-        [Routable]: A class that can be used to define a routable class
-
-"""
-
 
 class RoutableMeta(type):
     """This is a meta-class that converts all the methods that were marked by a route/path decorator into values on a
@@ -32,29 +24,23 @@ class RoutableMeta(type):
         ]
 
         attrs["_endpoints"] = endpoints
-        # Remove the _endpoint attribute from the class
         return cast(RoutableMeta, type.__new__(cls, name, bases, attrs))
 
 
 class Routable(metaclass=RoutableMeta):
-    """
-    - The Routable class is a base class for all the classes that are to be used as the base for the API.
-    - The Routable class is a metaclass that converts all the methods that were marked by a route/path decorator into
-        values on a class member called _endpoints that the Routable constructor then uses to add the endpoints to its
-        router.
+    """Base class for all classes the want class-based routing.
+
+    This Uses RoutableMeta as a metaclass and various decorators like @get or @post from the decorators module. The
+    decorators just mark a method as an endpoint. The RoutableMeta then converts those to a list of desired endpoints in
+    the _endpoints class method during class creation. The constructor constructs an APIRouter and adds all the routes
+    in the _endpoints to it so they can be added to an app via FastAPI.include_router or similar.
     """
 
-    _endpoints: List[EndpointDefinition] = [
-        # This is a list of all the endpoints that the class has defined
-        # TODO: This should be a list of EndpointDefinition objects
-    ]
+    _endpoints: List[EndpointDefinition] = []
 
-    def __init__(self) -> None:
-        # Create a router for the class
-        self.router = APIRouter()
-        # Loop through all the endpoints in the class
+    def __init__(self, *args, **kwargs) -> None:
+        self.router = APIRouter(*args, **kwargs)
         for endpoint in self._endpoints:
-            # The endpoint is a tuple of the endpoint name and the endpoint function
             self.router.add_api_route(
                 endpoint=partial(endpoint.endpoint, self),
                 **dataclasses.asdict(endpoint.args),
