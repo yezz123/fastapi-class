5651206fce2ab151a0744f6dcb1c422d4f34b96f
Yasser Tahiri <yasserth19@gmail.com>
2023-03-14 01:52:22 +0400

    remove `scripts` folder

--- Scripts/lint.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-set -x
-
-mypy --show-error-codes fastapi_class tests
--- Scripts/test.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-set -x
-
-echo "ENV=${ENV}"
-
-export PYTHONPATH=.
-pytest --cov=fastapi_class --cov=tests --cov-report=term-missing --cov-fail-under=80
