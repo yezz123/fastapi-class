dc2340a464555f8ad61d6597e7a164bac67ccfec
Yasser Tahiri <yasserth19@gmail.com>
2024-04-20 17:29:04 +0100

    🔨 Update requirements script to refresh dependencies

---
+++ scripts/requirements.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+refresh-lockfiles() {
+  echo "Updating requirements/*.txt files using uv"
+  find requirements/ -name '*.txt' ! -name 'all.txt' -type f -delete
+  uv pip compile requirements/linting.in -o requirements/linting.txt
+  uv pip compile requirements/testing.in -o requirements/testing.txt
+  uv pip compile pyproject.toml -o requirements/pyproject.txt
+  uv pip install -r requirements/all.txt
+}
+
+refresh-lockfiles
